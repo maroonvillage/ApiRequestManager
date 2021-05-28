8cc87fdabb1329ab28c7d99b76125318dd17634c
coleparsons <cole.parsons@go2vanguard.com>
2021-05-28 01:00:37 -0700

    Initial commit of newly added files

---
+++ Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using ApiRequestManager.Services;
+using ApiRequestManager.Interfaces;
+
+namespace ApiRequestManager
+{
+    public class Startup
+    {
+        private readonly IHttpClientBuilder _httpClientBuilter;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "apirequestmanager", Version = "v1" });
+            });
+            services.AddHttpClient();
+
+            //
+            services.AddMemoryCache();
+
+            services.RegisterServices();
+
+            _ = services.AddSingleton<IBasicIgApiService, BasicIgApiService>((dcx) =>
+            {
+                return new BasicIgApiService(new System.Net.Http.HttpClient(), Configuration);
+
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "apirequestmanager v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
